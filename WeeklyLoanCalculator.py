0628dfd79815173749b593c36e2f9ac278e49720
Johnwosu <W0524169@nscc.ca>
2025-10-05 18:07:02 -0300

    Create WeeklyLoanCalculator.py

---
+++ WeeklyLoanCalculator.py
@@ -0,0 +1,8 @@
+#---Weekly Loan Calculator--->
+
+# Develop a short term loan calculator program as a console application 
+# with the following specifications. Begin by designing your solution to this problem
+# in pseudocode, which will be submitted along with the program.
+# If A dollars are borrowed at r% interest compounded weekly to purchase 
+# something with weekly payments for n years, then the weekly payment is given by 
+# the formula:
