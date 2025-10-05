7f6080000b2a6b4786cd55ea86a6380a883e5242
John Nwosu <nwosujohnifeanyi@gmail.com>
2025-10-05 19:16:11 -0300

    workingonthepseudocode

--- WeeklyLoanCalculator.py
+++ WeeklyLoanCalculator.py
@@ -1,8 +1,11 @@
 #---Weekly Loan Calculator--->
 
-# Develop a short term loan calculator program as a console application 
-# with the following specifications. Begin by designing your solution to this problem
-# in pseudocode, which will be submitted along with the program.
-# If A dollars are borrowed at r% interest compounded weekly to purchase 
-# something with weekly payments for n years, then the weekly payment is given by 
-# the formula:
+#--Inputs will get the following:---
+# 1. The Loan 
+# 2. The interest rate (%)
+# 3. The number of years for the repayment
+
+#--Apply the Amortization formular to get the weekly payment--
+# 1. Weeks in a year = 52
+# 2. Weekly interest for the year = r% / 52
+# 3. weeklyPayment = weeklyInterest / (1- (1 + weeklyInterest))
